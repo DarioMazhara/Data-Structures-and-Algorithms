0f86aa6d66037683e4ae755601dfecfd6c1d8b68
arksenu <leizaro.fei@gmail.com>
2022-02-04 00:14:12 -0800

    Initial Commit

---
+++ Data Structures and Algorithms/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Data Structures and Algorithms
+//
+//  Created by Dario Mazhara on 2/4/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
